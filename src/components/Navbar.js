6423dabd18959c54c8c327b57ce7ee5f676176bf
Antony Nyagah <antonynyagah@Antonys-MacBook-Pro.local>
2023-08-27 15:22:40 -0500

    updated contact form

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav>
       <ul>
-        <li><a href="/">About Me</a></li>
-        <li><a href="/projects">Projects</a></li>
-        <li><a href="/contact">Contact</a></li>
-        <li><a href="https://resume.creddle.io/resume/14mulmr5kh6" target="_blank" rel="noopener noreferrer">Resume</a></li>
+        <li>
+          <Link to="/">About Me</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <a href="https://resume.creddle.io/resume/14mulmr5kh6" target="_blank" rel="noopener noreferrer">
+            Resume
+          </a>
+        </li>
       </ul>
     </nav>
   );
